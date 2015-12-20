f251eba920ced300d2b41116396e184714e7a23a
chain <chainsv@hotmail.com>
2015-12-20 08:15:43 +0700

    day2

--- app/src/main/java/com/maptech/chain/mytraffic/MainActivity.java
+++ app/src/main/java/com/maptech/chain/mytraffic/MainActivity.java
@@ -1,13 +1,65 @@
 package com.maptech.chain.mytraffic;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+import static com.maptech.chain.mytraffic.R.id.button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private ListView trafficListView;
+    private Button aboutMeButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // Bind Widdget
+        bindWidget();
+
+        //Button Controller
+
+        buttonController();
+
+    }       // Main Method
+
+    private void buttonController() {
+
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(),R.raw.effect_btn_long);
+                buttonMediaPlayer.start();
+
+                //Intent to web
+
+                Intent objIntent = new Intent(Intent.ACTION_VIEW);
+                objIntent.setData(Uri.parse("https://youtu.be/5LiD09JMqH8"));
+                startActivity(objIntent);
+
+            }
+        });
+
     }
-}
+
+
+    private void bindWidget() {
+
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButton = (Button) findViewById(R.id.button);
+
+
+    }
+
+
+}       // Main Class
